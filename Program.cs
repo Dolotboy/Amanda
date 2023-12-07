75c5ce0935a50a9551c359f113c0c5c3b99235ed
MaximeLepageUQAC <maxime-lepage@hotmail.ca>
2023-12-07 15:28:01 -0500

    feat: + Tokenization of sentences

--- Program.cs
+++ Program.cs
@@ -13,38 +13,29 @@ userInput = "Search whales";
 Bot.execute(intentRecognizer.RecognizeIntent(userInput));
 */
 
-var tokenizer = new Tokenizer();
+Tokenizer tokenizer = new Tokenizer();
 
-// Exemple d'utilisation
-string userInput = "Please play my favorite song!";
-List<string> tokens = tokenizer.Tokenize(userInput);
+List<string> sentences = new List<string>
+        {
+            "I love programming",
+            "Natural Language Processing is fascinating",
+            "Hello, how are you?",
+            "The quick brown fox jumps over the lazy dog",
+            "Coding is an art"
+        };
 
-Console.WriteLine("Tokens:");
-foreach (var token in tokens)
-{
-    Console.WriteLine(token);
-}
+int sequenceLength = 10;
 
-Console.WriteLine("\nToken Dictionary:");
-var tokenDictionary = tokenizer.GetTokenDictionary();
-foreach (var entry in tokenDictionary)
-{
-    Console.WriteLine($"Position {entry.Key}: {string.Join(", ", entry.Value)}");
-}
+List<int[]> sequences = tokenizer.CreateTextSequences(sentences, sequenceLength);
 
-// Exemple pour CreateTextSequences avec rembourrage jusqu'à la longueur spécifiée
-string inputSentence = "Please play my favorite song!";
-var sequences = tokenizer.CreateTextSequences(inputSentence, 10);
-
-Console.WriteLine("\nText Sequences:");
-foreach (var sequence in sequences)
-{
-    Console.WriteLine($"[{string.Join(", ", sequence)}]");
-}
-
-Console.WriteLine("\nWords Index:");
-var wordsIndex = tokenizer.GetWordsIndex();
-foreach (var entry in wordsIndex)
+Console.WriteLine("Words Index:");
+foreach (var entry in tokenizer.GetWordsIndex())
 {
     Console.WriteLine($"{entry.Key}: {entry.Value}");
+}
+
+Console.WriteLine("\nSequences:");
+foreach (var sequence in sequences)
+{
+    Console.WriteLine(string.Join(", ", sequence));
 }
--- Tokenizer.cs
+++ Tokenizer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 public class Tokenizer
@@ -9,72 +10,78 @@ public class Tokenizer
 
     public Tokenizer()
     {
-        tokenDictionary = new Dictionary<int, List<string>>();
-        wordsIndex = new Dictionary<string, int>();
+        // Initialisez les dictionnaires à partir des données sauvegardées ou créez de nouveaux dictionnaires vides
+        tokenDictionary = LoadTokenDictionary() ?? new Dictionary<int, List<string>>();
+        wordsIndex = LoadWordsIndex() ?? new Dictionary<string, int>();
     }
 
-    public List<string> Tokenize(string input)
+    public List<string> Tokenize(List<string> inputs)
     {
-        // Divisez la phrase en mots (tokens) en prenant en compte les ponctuations
-        string[] words = input.Split(new[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
-
-        // Ajoutez les tokens au dictionnaire avec leur position dans la phrase
-        for (int i = 0; i < words.Length; i++)
+        foreach (var input in inputs)
         {
-            string word = words[i].ToLower(); // Convertir en minuscules pour éviter la sensibilité à la casse
+            // Divisez la phrase en mots (tokens) en prenant en compte les ponctuations
+            string[] words = input.Split(new[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
 
-            // Ajouter le mot à l'index s'il n'est pas déjà présent
-            if (!wordsIndex.ContainsKey(word))
+            // Ajoutez les tokens au dictionnaire avec leur position dans la phrase
+            for (int i = 0; i < words.Length; i++)
             {
-                wordsIndex[word] = wordsIndex.Count + 1; // Index commence à 1, 0 peut être utilisé pour les mots non présents dans l'index
-            }
+                string word = words[i].ToLower(); // Convertir en minuscules pour éviter la sensibilité à la casse
 
-            // Ajouter l'index du mot au dictionnaire des tokens
-            if (!tokenDictionary.ContainsKey(i))
-            {
-                tokenDictionary[i] = new List<string>();
-            }
+                // Ajouter le mot à l'index s'il n'est pas déjà présent
+                if (!wordsIndex.ContainsKey(word))
+                {
+                    wordsIndex[word] = wordsIndex.Count + 1; // Index commence à 1, 0 peut être utilisé pour les mots non présents dans l'index
+                }
 
-            tokenDictionary[i].Add(word);
+                // Ajouter l'index du mot au dictionnaire des tokens
+                if (!tokenDictionary.ContainsKey(i))
+                {
+                    tokenDictionary[i] = new List<string>();
+                }
+
+                tokenDictionary[i].Add(word);
+            }
         }
 
+        // Sauvegardez les dictionnaires
+        SaveTokenDictionary(tokenDictionary);
+        SaveWordsIndex(wordsIndex);
+
         // Retournez la liste de tokens
-        return words.ToList();
+        return inputs.SelectMany(input => input.Split(new[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Select(word => word.ToLower())).ToList();
     }
 
-    public Dictionary<int, List<string>> GetTokenDictionary()
-    {
-        // Retourne le dictionnaire de tokens
-        return tokenDictionary;
-    }
-
-    public List<int[]> CreateTextSequences(string sentence, int sequenceLength)
+    public List<int[]> CreateTextSequences(List<string> sentences, int sequenceLength)
     {
         // Crée des séquences de texte à partir des tokens avec un rembourrage jusqu'à la longueur spécifiée
         List<int[]> sequences = new List<int[]>();
 
-        string[] words = sentence.Split(new[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
-
-        foreach (var word in words)
+        foreach (var sentence in sentences)
         {
-            string lowerWord = word.ToLower();
+            string[] words = sentence.Split(new[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
 
-            // Ajouter le mot à l'index s'il n'est pas déjà présent
-            if (!wordsIndex.ContainsKey(lowerWord))
-            {
-                wordsIndex[lowerWord] = wordsIndex.Count + 1; // Index commence à 1, 0 peut être utilisé pour les mots non présents dans l'index
-            }
-        }
-
-        for (int i = 0; i < sequenceLength; i++)
-        {
             int[] sequence = new int[sequenceLength];
 
-            if (i < words.Length)
+            for (int i = 0; i < sequenceLength; i++)
             {
-                string word = words[i].ToLower();
-                int index = wordsIndex[word];
-                sequence[i] = index;
+                if (i < words.Length)
+                {
+                    string word = words[i].ToLower();
+
+                    // Ajouter le mot à l'index s'il n'est pas déjà présent
+                    if (!wordsIndex.ContainsKey(word))
+                    {
+                        wordsIndex[word] = wordsIndex.Count + 1; // Index commence à 1, 0 peut être utilisé pour les mots non présents dans l'index
+                    }
+
+                    int index = wordsIndex[word];
+                    sequence[i] = index;
+                }
+                else
+                {
+                    // Remplir avec des zéros si la séquence est plus courte que la longueur spécifiée
+                    sequence[i] = 0;
+                }
             }
 
             sequences.Add(sequence);
@@ -88,4 +95,52 @@ public class Tokenizer
         // Retourne l'index des mots
         return wordsIndex;
     }
+
+    private Dictionary<int, List<string>> LoadTokenDictionary()
+    {
+        // Implémentez la logique pour charger le dictionnaire de tokens depuis le stockage persistant (fichier, base de données, etc.)
+        // Retournez null s'il n'y a pas de données sauvegardées
+        // Exemple : return LoadDataFromFile<Dictionary<int, List<string>>>("tokenDictionary.json");
+        return null;
+    }
+
+    private void SaveTokenDictionary(Dictionary<int, List<string>> data)
+    {
+        // Implémentez la logique pour sauvegarder le dictionnaire de tokens dans le stockage persistant
+        // Exemple : SaveDataToFile("tokenDictionary.json", data);
+    }
+
+    private Dictionary<string, int> LoadWordsIndex()
+    {
+        // Implémentez la logique pour charger l'index des mots depuis le stockage persistant
+        // Retournez null s'il n'y a pas de données sauvegardées
+        // Exemple : return LoadDataFromFile<Dictionary<string, int>>("wordsIndex.json");
+        return null;
+    }
+
+    private void SaveWordsIndex(Dictionary<string, int> data)
+    {
+        // Implémentez la logique pour sauvegarder l'index des mots dans le stockage persistant
+        // Exemple : SaveDataToFile("wordsIndex.json", data);
+    }
+
+    // Ajoutez d'autres méthodes de chargement et de sauvegarde si nécessaire
+
+    // Exemple générique de méthode de chargement depuis un fichier
+    // private T LoadDataFromFile<T>(string filePath)
+    // {
+    //     if (File.Exists(filePath))
+    //     {
+    //         string json = File.ReadAllText(filePath);
+    //         return JsonConvert.DeserializeObject<T>(json);
+    //     }
+    //     return default(T);
+    // }
+
+    // Exemple générique de méthode de sauvegarde dans un fichier
+    // private void SaveDataToFile<T>(string filePath, T data)
+    // {
+    //     string json = JsonConvert.SerializeObject(data);
+    //     File.WriteAllText(filePath, json);
+    // }
 }
