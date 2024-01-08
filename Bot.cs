f8ab90e4fdd2b2e58c59a8d9c270e7301bacde08
MaximeLepageUQAC <maxime-lepage@hotmail.ca>
2024-01-08 16:23:37 -0500

    feat: Add WIT cURL call

--- Bot.cs
+++ Bot.cs
@@ -4,42 +4,67 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Amanda
 {
     public class Bot
     {
-
-        public Bot()
+        public static async void execute(string userInput)
         {
+            var url = "https://api.wit.ai/message?v=20240108&q=" + userInput;
 
-        }
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
 
-        public static void execute(Intent intent)
-        {
-            Console.WriteLine($"Recognized Intent: {intent.intent}");
-            switch (intent.intent)
+            httpRequest.Accept = "application/json";
+            httpRequest.Headers["Authorization"] = "Bearer KRX3H5YNCW26BCBLVEA5TAXUXZA5KKFJ";
+
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
-                case IntentType.OpenApplication:
-                    Console.WriteLine("Openning: " + intent.application.Name);
-                    Process.Start(intent.application.ExecutablePath);
-                    break;
-                case IntentType.PlayMusic:
-                    Console.WriteLine("Playing music: " + intent.concernedObject);
-                    break;
-                case IntentType.PlayMovie:
-                    Console.WriteLine("Playing movie:" + intent.concernedObject);
-                    break;
-                case IntentType.Search:
-                    Console.WriteLine("Searching:" + intent.concernedObject);
-                    var ps = new ProcessStartInfo("https://www.google.com/search?q=" + intent.concernedObject)
+                var result = JsonConvert.DeserializeObject<JObject>(streamReader.ReadToEnd());
+
+                foreach (var intentProperty in result["intents"].Children<JProperty>())
+                {
+                    var intentName = intentProperty.Name; // Nom de l'entité
+                    var intentValue = intentProperty.Value; // Valeur de l'entité
+
+                    Console.WriteLine($"Entité : {intentName}, Valeur : {intentValue}");
+
+                    // Si la valeur est un tableau, vous pouvez le parcourir de la même manière
+                    if (intentValue is JArray entityArray)
                     {
-                        UseShellExecute = true,
-                        Verb = "open"
-                    };
-                    Process.Start(ps);
-                    break;
+                        foreach (var item in entityArray)
+                        {
+                            // Faire quelque chose avec chaque élément du tableau
+                        }
+                    }
+                }
+
+                foreach (var entityProperty in result["entities"].Children<JProperty>())
+                {
+                    var entityName = entityProperty.Name; // Nom de l'entité
+                    var entityValue = entityProperty.Value; // Valeur de l'entité
+
+                    Console.WriteLine($"Entité : {entityName}, Valeur : {entityValue}");
+
+                    // Si la valeur est un tableau, vous pouvez le parcourir de la même manière
+                    if (entityValue is JArray entityArray)
+                    {
+                        foreach (var item in entityArray)
+                        {
+                            // Faire quelque chose avec chaque élément du tableau
+                        }
+                    }
+                }
             }
+
+            //Console.WriteLine(httpResponse.StatusCode);
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,26 +1,12 @@
 ﻿// Example usage
 using Amanda;
 
-
-var intentRecognizer = new IntentRecognizer();
-
 string userInput = "";
 
 //userInput = "Please play my favorite song which is old town road";
 //userInput = "Please open streamlabs";
-userInput = "Search whales";
+//userInput = "Search whales";
+userInput = "Ouvrir Steam";
 
-Bot.execute(intentRecognizer.RecognizeIntent(userInput));
+Bot.execute(userInput);
 
-
-void trainAI()
-{
-    tokenizer.LearnWordsFromJsonFile(@"E:\Programmation\Apps\Amanda\Trainings\words1.json");
-    tokenizer.LearnWordsFromJsonFile(@"E:\Programmation\Apps\Amanda\Trainings\words2.json");
-    tokenizer.LearnWordsFromJsonFile(@"E:\Programmation\Apps\Amanda\Trainings\words3.json");
-    tokenizer.LearnWordsFromJsonFile(@"E:\Programmation\Apps\Amanda\Trainings\words4.json");
-    tokenizer.LearnSentencesWithIntentFromJson(@"E:\Programmation\Apps\Amanda\Trainings\OpenApplicationIntents.json", IntentType.OpenApplication);
-    tokenizer.LearnSentencesWithIntentFromJson(@"E:\Programmation\Apps\Amanda\Trainings\PlayMusicIntents.json", IntentType.PlayMusic);
-    tokenizer.LearnSentencesWithIntentFromJson(@"E:\Programmation\Apps\Amanda\Trainings\PlayMovieIntents.json", IntentType.PlayMovie);
-    tokenizer.LearnSentencesWithIntentFromJson(@"E:\Programmation\Apps\Amanda\Trainings\SearchIntents.json", IntentType.Search);
-}
